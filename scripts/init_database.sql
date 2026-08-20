6648c9589801c154049dacef50c5b5e59a4ba812
HassanRaiss <hassanraiss46@gmail.com>
2026-08-20 21:40:37 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,25 @@
+/*
+===============================================================================
+Script Purpose: Initialize DataWarehouse Database
+Description: 
+    This script sets up the 'DataWarehouse' database environment for a multi-layered 
+    data architecture (Bronze, Silver, Gold).
+    
+    Key Actions:
+    1. Checks if the database already exists and drops it to allow a fresh setup.
+    2. Creates the 'DataWarehouse' database.
+    3. Sets up three key schemas representing the data warehouse layers:
+       - bronze: Raw, unprocessed source data
+       - silver: Cleaned, transformed, and normalized data
+       - gold:  Aggregated business-ready data for reporting/analytics
+
+WARNING:
+    Executing this script will drop the existing 'DataWarehouse' database 
+    and permanently delete all stored data.
+===============================================================================
+*/
+
+
 -- Check if the 'DataWarehouse' database already exists before creating it.
 -- If it exists, drop it to ensure a clean setup (remove this section if you wish to keep existing data).
 IF EXISTS (SELECT name FROM sys.databases WHERE name = N'DataWarehouse')
